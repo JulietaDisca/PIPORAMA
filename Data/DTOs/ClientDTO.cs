22bd75e44d12f42d570069686569e22de37f3d38
412157-1w1-Guemberena-Jeronimo <412157@tecnicatura.frc.utn.edu.ar>
2025-10-24 23:17:29 -0300

    Commit final final

---
+++ Data/DTOs/ClientDTO.cs
@@ -0,0 +1,20 @@
+﻿using TP_ProgramaciónII_PIPORAMA.Data.DTOs;
+
+namespace TP_ProgramaciónII_PIPORAMA.DTOs
+{
+    public class ClientDTO
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public int IdTipoCliente { get; set; }
+        public int IdBarrio { get; set; }
+        public int IdContacto { get; set; }
+        public ClientTypeDTO TipoCliente { get; set; }
+        public NeighborhoodDTO Barrio { get; set; }
+        public ContactDTO Contacto { get; set; }
+        
+    }
+
+    
+}
---
+++ Repositories/Interfaces/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Identity.Client;
+using TP_ProgramaciónII_PIPORAMA.Data.Models;
+using TP_ProgramaciónII_PIPORAMA.DTOs;
+
+namespace TP_ProgramaciónII_PIPORAMA.Interfaces
+{
+    public interface IClientRepository
+    {
+        Task<List<Cliente>> GetAllClientsAsync();
+        Task<Cliente?> GetClientByIdAsync(int id);
+        Task<Cliente> AddClientAsync(Cliente client);
+        Task <Cliente> UpdateClientAsync(Cliente client);
+        Task<bool> DeleteClientAsync(int clientId);
+    }
+}
