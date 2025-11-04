31a096cd2829972d5af36d7d22c768593a05071e
Juan Manuel Torrejón <421483@tecnicatura.frc.utn.edu.ar>
2025-11-04 16:14:30 -0300

    GetAll para combos de front

---
+++ Repositories/Implementations/AdditionalRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TP_ProgramaciónII_PIPORAMA.Data.Models;
+using TP_ProgramaciónII_PIPORAMA.Repositories.Interfaces;
+
+namespace TP_ProgramaciónII_PIPORAMA.Repositories.Implementations
+{
+    public class AdditionalRepository : IAdditionalRepository
+    {
+        private PIPORAMAContext _context;
+        public AdditionalRepository(PIPORAMAContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Barrio>> GetAllBarrios()
+        {
+            return await _context.Barrios.ToListAsync();
+        }
+
+        public async Task<IEnumerable<SalaButaca>> GetAllSalasButacas()
+        {
+            return await _context.SalaButacas.Include(sb => sb.IdSalaNavigation).Include(sb => sb.IdButacaNavigation).ToListAsync();
+        }
+
+        public async Task<IEnumerable<EstadosCompra>> GetAllEstadosCompra()
+        {
+            return await _context.EstadosCompras.ToListAsync();
+        }
+
+        public async Task<IEnumerable<FormaCompra>> GetAllFormasCompra()
+        {
+            return await _context.FormaCompras.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Funcione>> GetAllFunciones()
+        {
+            return await _context.Funciones.Include(f => f.IdPeliculaNavigation).Include(f=> f.IdSalaNavigation).ToListAsync();
+        }
+
+        public async Task<IEnumerable<MediosPago>> GetAllMediosPago()
+        {
+            return await _context.MediosPagos.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Pelicula>> GetAllPeliculas()
+        {
+            return await _context.Peliculas.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Promocione>> GetAllPromociones()
+        {
+            return await _context.Promociones.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TiposCliente>> GetAllTipoClientes()
+        {
+            return await _context.TiposClientes.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TiposContacto>> GetAllTipoContactos()
+        {
+            return await _context.TiposContactos.ToListAsync();
+        }
+    }
+}
---
+++ Services/Interfaces/IAdditionalService.cs
@@ -0,0 +1,20 @@
+﻿using TP_ProgramaciónII_PIPORAMA.Data.DTOs.Additional;
+using TP_ProgramaciónII_PIPORAMA.Data.DTOs.DetailInvoice;
+using TP_ProgramaciónII_PIPORAMA.Data.Models;
+
+namespace TP_ProgramaciónII_PIPORAMA.Services.Interfaces
+{
+    public interface IAdditionalService
+    {
+        Task<IEnumerable<Barrio>> GetAllBarrios();
+        Task<IEnumerable<TiposCliente>> GetAllTipoClientes();
+        Task<IEnumerable<TiposContacto>> GetAllTipoContactos();
+        Task<IEnumerable<Promocione>> GetAllPromociones();
+        Task<IEnumerable<RoomSeatDTO>> GetAllSalasButacas();
+        Task<IEnumerable<FilmDTO>> GetAllPeliculas();
+        Task<IEnumerable<FunctionDTO>> GetAllFunciones();
+        Task<IEnumerable<MediosPago>> GetAllMediosPago();
+        Task<IEnumerable<EstadosCompra>> GetAllEstadosCompra();
+        Task<IEnumerable<FormaCompra>> GetAllFormasCompra();
+    }
+}
