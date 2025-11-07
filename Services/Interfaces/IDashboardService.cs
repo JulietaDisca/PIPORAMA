ecc37b49075e88a5f1da2e0c197da224a28c9a34
juli.disca <discajulieta@gmail.com>
2025-11-07 16:08:21 -0300

    Consultas dashboard

---
+++ Services/Interfaces/IDashboardService.cs
@@ -0,0 +1,33 @@
+﻿using TP_ProgramaciónII_PIPORAMA.Data.Models;
+
+namespace TP_ProgramaciónII_PIPORAMA.Services.Interfaces
+{
+    public interface IDashboardService
+    {
+        Task<IEnumerable<EntradasRecaudacionXDiaResult>> GetEntradasRecaudaciónXDia();
+
+        Task<IEnumerable<RecaudacionXPeliculaResult>> GetRecaudaciónTotalXPelícula();
+
+        Task<IEnumerable<PromedioEntradasPorSalaResult>> GetPromedioEntradasVendidasXSala();
+
+        Task<IEnumerable<PeliculasMasVistasResult>> GetPelículasMásVistas();
+
+        Task<IEnumerable<FuncionesXHorarioResult>> GetCantidadFuncionesPorFranjaHoraria();
+
+        Task<IEnumerable<ProximasFuncionesResult>> GetPróximasFunciones();
+
+        Task<IEnumerable<ClientesFrecuentesResult>> GetClientesFrecuentes();
+
+        Task<IEnumerable<ConsumiblesMasVendidosResult>> GetProductosMásVendidos();
+
+        Task<IEnumerable<RecaudacónTotalCombosResult>> GetRecaudacionTotalCombos();
+
+        Task<IEnumerable<TotalEntradasVendidasResult>> GetTotalEntradasVendidas();
+
+        Task<IEnumerable<RecaudacionTotalResult>> GetRecaudacionTotal();
+
+        Task<IEnumerable<PeliculasEnCarteleraResult>> GetPeliculasEnCartelera();
+
+        Task<IEnumerable<TotalClientesRegistradosResult>> GetTotalClientesRegistrados();
+    }
+}
